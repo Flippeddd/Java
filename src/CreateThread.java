69dc88e8b7c47f30f25bb20864e3b1458f8989d8
bofengding <d.flippeddd@gmail.com>
2020-08-26 18:08:33 -0400

    Initial commit

---
+++ src/CreateThread.java
@@ -0,0 +1,33 @@
+
+public class CreateThread {
+    public static void main(String[] args) {
+        /*  extends Thread class */
+        Thread thread1 = new Thread1();
+        thread1.start();
+        /*  implements Runnable interface */
+        Thread thread2 = new Thread(new myRunnable());
+        thread2.start();
+        /*  lambda */
+        Thread thread3 = new Thread(() -> {
+            System.out.println("Thread3 is running");
+            System.out.println("Thread3 end");
+        });
+        thread3.start();
+    }
+}
+
+class Thread1 extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Thread1 is running");
+        System.out.println("Thread1 end");
+    }
+}
+
+class myRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Thread2 is running");
+        System.out.println("Thread2 end");
+    }
+}
